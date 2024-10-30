5d71f4c7814043ae190fb24d6674c1eaa82f09e5
Ifrah-Fahim <ifrahfahim98@gmail.com>
2024-10-29 21:38:02 -0700

    Add files via upload

---
+++ pf ifrah.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// 5! 5*4*3*2*1= 120
+	int n;
+	long factorial = 1.0;
+	cout<<"enter a factorial";
+	cin>>n;
+	if (n < 0)
+	cout<<"error! factorail of a negative number doesn't exist";
+	
+	else {
+	int i=1;
+	do {
+		cout<<"i"<<endl;
+		i++;
+	}
+	while(i<=n);
+	cout<<"factorial of"<<n<<"="<<factorial;
+	
+	}
+	return 0;
+}
